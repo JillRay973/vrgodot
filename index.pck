ac2638cec7effb9cca7d2c555ff96cf9d40878c2
JillRay973 <143532365+JillRay973@users.noreply.github.com>
2024-10-31 11:22:46 -0400

    fix

--- index.pck
+++ index.pck
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe7b4ff452a5af396f458fc37c7f88c93975790bd7192a909a2f7cf43d8e498a
-size 115768592
+oid sha256:0a103f3c47a1104bced621ecb419435a5ffec6752aafefbdaf3c017268ef21c0
+size 53218944
--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730335592|110716640859';
+const CACHE_VERSION = '1730388155|708508435';
 /** @type {string} */
 const CACHE_PREFIX = 'iiSXRtemplate-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
